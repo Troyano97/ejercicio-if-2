dd3d6d4ef05f2316eaf321fb182c2fa9a9614b17
¨troyano97¨ <ivanferreyradaniel@gmail.com>
2023-04-13 11:53:10 -0300

    primer commit

---
+++ script.js
@@ -0,0 +1,10 @@
+
+const VALOR_BITCOIN_DOLAR = "39.685,40";
+const ETHEREUM_CAMBIO_BITCOIN_UNO = "13,5400";
+
+let bitcoinCambiar = prompt ("Ingrese la cantidad de bitcoin a cambiar");
+//let seleccionMonedaDolar = confirm("¿Queres cambiar a dolares?");
+//let seleccionMonedaEth = confirm ("¿Queres cambiar a ETHEREUM?");
+if (seleccionMonedaDolar = confirm ("¿Queres cambiar a dolares?") || (seleccionMonedaEth = confirm ("¿Queres cambiar a ETHEREUM?"))) 
+{
+}
